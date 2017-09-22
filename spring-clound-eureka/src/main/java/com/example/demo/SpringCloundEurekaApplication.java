85f1d6f619dae04cbd5d8a0a834f53a0bf7d009d
qfttxq <qfttxq@163.com>
2017-09-22 23:04:30 +0800

    设置eureka服务器配置参数

--- spring-clound-eureka/src/main/java/com/example/demo/SpringCloundEurekaApplication.java
+++ spring-clound-eureka/src/main/java/com/example/demo/SpringCloundEurekaApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableEurekaServer
 public class SpringCloundEurekaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloundEurekaApplication.class, args);
 	}
