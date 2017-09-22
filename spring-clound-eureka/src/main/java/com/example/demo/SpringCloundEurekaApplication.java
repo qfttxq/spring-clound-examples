be9335d3eb3cbba143d9cc314ec4ef5f8af3fdae
qfttxq <qfttxq@163.com>
2017-09-22 22:44:30 +0800

    init

---
+++ spring-clound-eureka/src/main/java/com/example/demo/SpringCloundEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableEurekaServer
+public class SpringCloundEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloundEurekaApplication.class, args);
+	}
+}
